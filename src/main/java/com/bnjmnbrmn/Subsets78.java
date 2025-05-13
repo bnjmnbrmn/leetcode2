b74473fce9fc1b254ef588873d30c305d0e8ee1d
Benjamin Berman <baberman@mmm.com>
2025-05-13 13:02:21 -0400

    Add solutions to Subsets78

--- src/main/java/com/bnjmnbrmn/Subsets78.java
+++ src/main/java/com/bnjmnbrmn/Subsets78.java
@@ -2,6 +2,7 @@ package com.bnjmnbrmn;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class Subsets78 {
 
@@ -75,23 +76,154 @@ public class Subsets78 {
 //    }
 
     //backtracking 2
+//    public List<List<Integer>> subsets(int[] nums) {
+//        return subsetsOfSuffixThatAlsoInclude(nums, 0, new ArrayList<>());
+//    }
+//
+//    private List<List<Integer>> subsetsOfSuffixThatAlsoInclude(int[] nums, int suffixStart, List<Integer> toInclude) {
+//        List<List<Integer>> toRet = List.of(toInclude);
+//        for (int i = suffixStart; i < nums.length; i++) {
+//            List<Integer> toIncludePlusIth = concat(toInclude, List.of(nums[i]));
+//            List<List<Integer>> suffixSubsetsPlus = subsetsOfSuffixThatAlsoInclude(nums, i + 1, toIncludePlusIth);
+//            toRet = concat(toRet,  suffixSubsetsPlus);
+//        }
+//        return toRet;
+//    }
+//
+//    private <T> List<T> concat(List<T> listA, List<T> listB) {
+//        List<T> toRet = new ArrayList<>(listA);
+//        toRet.addAll(listB);
+//        return toRet;
+//    }
+
+    //backtracking 3
+//    int[] nums;
+//    public List<List<Integer>> subsets(int[] nums) {
+//        this.nums = nums;
+//        return suffixSubsets(0);
+//    }
+//
+//    private List<List<Integer>> suffixSubsets(int suffixStart) {
+//        return suffixesSubsetsPlus(List.of(), suffixStart);
+//    }
+//
+//    private List<List<Integer>> suffixesSubsetsPlus(List<Integer> toInclude, int suffixStart) {
+//
+//        List<List<Integer>> suffixesSubsetsPlus = new ArrayList<>();
+//        suffixesSubsetsPlus.add(toInclude);
+//
+//        for (int i = suffixStart; i < nums.length; i++) {
+//            List<Integer> newToInclude = concatIntegerLists(toInclude, List.of(nums[i]));
+//            int newSuffixStart = i+1;
+//            suffixesSubsetsPlus.addAll(suffixesSubsetsPlus(newToInclude, newSuffixStart));
+//        }
+//        return suffixesSubsetsPlus;
+//    }
+//
+//    private List<Integer> concatIntegerLists(List<Integer> listA, List<Integer> listB) {
+//        List<Integer> toRet = new ArrayList<>(listA);
+//        toRet.addAll(listB);
+//        return toRet;
+//    }
+
+
+    //backtracking 4
+//    int[] nums;
+//    public List<List<Integer>> subsets(int[] nums) {
+//        this.nums = nums;
+//        return suffixSubsets(0);
+//    }
+//
+//    private List<List<Integer>> suffixSubsets(int suffixStart) {
+//        return suffixesSubsetsPlus(List.of(), suffixStart);
+//    }
+//
+//    private List<List<Integer>> suffixesSubsetsPlus(List<Integer> toInclude, int suffixStart) {
+//
+//        return concatListsOfIntegerLists(List.of(toInclude), IntStream.range(suffixStart, nums.length).boxed().flatMap(i ->
+//                suffixesSubsetsPlus(concatIntegerLists(toInclude, List.of(nums[i])), i + 1).stream())
+//                .toList());
+//    }
+//
+//    private List<List<Integer>> concatListsOfIntegerLists(List<List<Integer>> listA, List<List<Integer>> listB) {
+//        List<List<Integer>> toRet = new ArrayList<>(listA);
+//        toRet.addAll(listB);
+//        return toRet;
+//    }
+//
+//    private List<Integer> concatIntegerLists(List<Integer> listA, List<Integer> listB) {
+//        List<Integer> toRet = new ArrayList<>(listA);
+//        toRet.addAll(listB);
+//        return toRet;
+//    }
+
+//    public List<List<Integer>> subsets(int[] nums) {
+//        return subsets(Arrays.stream(nums).boxed().toList());
+//    }
+//
+//    private List<List<Integer>> subsets(List<Integer> nums) {
+//        return concat(List.of(List.of()),subsetsPlus( nums, List.of()));
+//    }
+//
+//    private List<List<Integer>> subsetsPlus(List<Integer> nums, List<Integer> toInclude) {
+//
+//        if (nums.isEmpty())
+//            return List.of();
+//
+//        Integer head = head(nums);
+//        List<Integer> tail = tail(nums);
+//        return concat(
+//                List.of(concat(toInclude, List.of(head))),
+//                subsetsPlus(tail, concat(toInclude,List.of(head))),
+//                subsetsPlus(tail, toInclude)
+//        );
+//    }
+//
+//    private List<Integer> tail(List<Integer> nums) {
+//        return nums.subList(1, nums.size());
+//    }
+//
+//    private Integer head(List<Integer> nums) {
+//        return nums.getFirst();
+//    }
+//
+//    private <T> List<T> concat(List<T> listA, List<T> listB) {
+//        List<T> toRet = new ArrayList<>(listA);
+//        toRet.addAll(listB);
+//        return toRet;
+//    }
+//
+//    private <T> List<T> concat(List<T> listA, List<T> listB, List<T> listC) {
+//        List<T> toRet = new ArrayList<>(listA);
+//        toRet.addAll(listB);
+//        toRet.addAll(listC);
+//        return toRet;
+//    }
+
+
+    int[] nums;
     public List<List<Integer>> subsets(int[] nums) {
-        return subsetsOfSuffixThatAlsoInclude(nums, 0, new ArrayList<>());
+        this.nums = nums;
+        return suffixesSubsetsPlus(List.of(), 0);
     }
 
-    private List<List<Integer>> subsetsOfSuffixThatAlsoInclude(int[] nums, int suffixStart, List<Integer> toInclude) {
-        List<List<Integer>> toRet = List.of(toInclude);
-        for (int i = suffixStart; i < nums.length; i++) {
-            List<Integer> toIncludePlusIth = concat(toInclude, List.of(nums[i]));
-            List<List<Integer>> suffixSubsetsPlus = subsetsOfSuffixThatAlsoInclude(nums, i + 1, toIncludePlusIth);
-            toRet = concat(toRet,  suffixSubsetsPlus);
-        }
+    private List<List<Integer>> suffixesSubsetsPlus(List<Integer> toInclude, int suffixStart) {
+
+        return prepend(toInclude, IntStream.range(suffixStart, nums.length).boxed().flatMap(i ->
+                suffixesSubsetsPlus(append(toInclude, nums[i]), i + 1).stream())
+                .toList());
+    }
+
+    private <T> List<T> prepend(T t, List<T> list) {
+        List<T> toRet = new ArrayList<>();
+        toRet.add(t);
+        toRet.addAll(list);
         return toRet;
     }
 
-    private <T> List<T> concat(List<T> listA, List<T> listB) {
-        List<T> toRet = new ArrayList<>(listA);
-        toRet.addAll(listB);
+    private <T> List<T> append(List<T> list, T t) {
+        List<T> toRet = new ArrayList<>(list);
+        toRet.add(t);
         return toRet;
     }
 
